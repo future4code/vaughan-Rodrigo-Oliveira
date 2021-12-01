a95d154c0f12214858580b4b6e2081432d57b9ee
Kord <rodrigokord@gmail.com>
2021-12-01 16:02:55 -0300

    mensagem-descrevendo-a-mudança-feita

--- modulo1/operadores/template_javascript/index.js
+++ modulo1/operadores/template_javascript/index.js
@@ -23,11 +23,30 @@ console.log(soma)
 
 // exercicio 1
 
-const idade = prompt ("Digite sua idade" )
+const idade  = prompt ("Digite sua idade" )
 const idadeAmi = prompt  ("Idade da sua melhor amiga")
 
-const dif = idade 28 => idadeAmi 34 
+const dif = idade >= idadeAmi
+console.log ("Sua idade é maior que do sua melhor amiga?"), idade >= idadeAmi 
 
-console.log (Sua idade é maior que do sua melhor amiga?, idade => idadeami)
 
+const numeroPar = prompt ("Digite um numero par")
+const divisao = numeroPar % 2
 
+console.log (divisao)
+
+// Aqui o resultado sempre dando 0
+// Se colocar um numero impar, o resultado sempre sera 1
+
+let = num1 = prompt ("Digite um numero")
+let = num2 = prompt ("Digite outro numero")
+
+const numeroMaior = num1 > num2
+const numeroIgual = num1 == num2
+const numDivi = num1 / num2
+const numeDividi = num2 / num1
+
+console.log ("O primeiro numero é maior que o segundo?", numeroMaior)
+console.log ("O primeiro numero é igual o segundo?", numeroIgual)
+console.log ("O primeiro numero é divisivel pelo segundo?", numDivi)
+console.log ("O segundo numero é divisivel pelo primeiro", numeDividi)
